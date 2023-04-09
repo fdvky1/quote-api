5e14a6b8cb8aac185746200301fa4de6e1d78594
fdvky1 <fdvkey@gmail.com>
2023-04-09 07:44:34 +0700

    update methods/generate.js

--- methods/generate.js
+++ methods/generate.js
@@ -1,50 +1,52 @@
-const {
-  QuoteGenerate
-} = require('../utils')
-const { createCanvas, loadImage } = require('canvas')
-const sharp = require('sharp')
+const { QuoteGenerate } = require("../utils");
+const { createCanvas, loadImage } = require("canvas");
+const sharp = require("sharp");
 
 // https://codepen.io/jreyesgs/pen/yadmge
 const addLight = (color, amount) => {
-  const cc = parseInt(color, 16) + amount
-  let c = (cc > 255) ? 255 : (cc)
-  c = (c.toString(16).length > 1) ? c.toString(16) : `0${c.toString(16)}`
-  return c
-}
+  const cc = parseInt(color, 16) + amount;
+  let c = cc > 255 ? 255 : cc;
+  c = c.toString(16).length > 1 ? c.toString(16) : `0${c.toString(16)}`;
+  return c;
+};
 
 const normalizeColor = (color) => {
-  const canvas = createCanvas(0, 0)
-  const canvasCtx = canvas.getContext('2d')
+  const canvas = createCanvas(0, 0);
+  const canvasCtx = canvas.getContext("2d");
 
-  canvasCtx.fillStyle = color
-  color = canvasCtx.fillStyle
+  canvasCtx.fillStyle = color;
+  color = canvasCtx.fillStyle;
 
-  return color
-}
+  return color;
+};
 
 const lighten = (color, amount) => {
-  color = (color.indexOf('#') >= 0) ? color.substring(1, color.length) : color
-  amount = parseInt((255 * amount) / 100)
-  color = `#${addLight(color.substring(0, 2), amount)}${addLight(color.substring(2, 4), amount)}${addLight(color.substring(4, 6), amount)}`
+  color = color.indexOf("#") >= 0 ? color.substring(1, color.length) : color;
+  amount = parseInt((255 * amount) / 100);
+  color = `#${addLight(color.substring(0, 2), amount)}${addLight(
+    color.substring(2, 4),
+    amount
+  )}${addLight(color.substring(4, 6), amount)}`;
 
-  return color
-}
+  return color;
+};
 
 module.exports = async (parm) => {
   // console.log(JSON.stringify(parm, null, 2))
-  if (!parm) return { error: 'query_empty' }
-  if (!parm.messages || parm.messages.length < 1) return { error: 'messages_empty' }
+  if (!parm) return { error: "query_empty" };
+  if (!parm.messages || parm.messages.length < 1)
+    return { error: "messages_empty" };
 
-  let botToken = parm.botToken || process.env.BOT_TOKEN
+  let botToken = parm.botToken || process.env.BOT_TOKEN;
 
-  const quoteGenerate = new QuoteGenerate(botToken)
+  const quoteGenerate = new QuoteGenerate(botToken);
 
-  parm.backgroundColor = normalizeColor(parm.backgroundColor)
+  parm.backgroundColor = normalizeColor(parm.backgroundColor);
 
-  const quoteImages = []
+  const quoteImages = [];
 
   for (const key in parm.messages) {
-    const message = parm.messages[key]
+    const message = parm.messages[key];
 
     if (message) {
       const canvasQuote = await quoteGenerate.generate(
@@ -54,115 +56,148 @@ module.exports = async (parm) => {
         parm.height,
         parm.scale,
         parm.emojiBrand
-      )
+      );
 
-      quoteImages.push(canvasQuote)
+      quoteImages.push(canvasQuote);
     }
   }
 
   if (quoteImages.length === 0) {
     return {
-      error: 'empty_messages'
-    }
+      error: "empty_messages",
+    };
   }
 
-  let canvasQuote
+  let canvasQuote;
 
   if (quoteImages.length > 1) {
-    let width = 0
-    let height = 0
+    let width = 0;
+    let height = 0;
 
     for (let index = 0; index < quoteImages.length; index++) {
-      if (quoteImages[index].width > width) width = quoteImages[index].width
-      height += quoteImages[index].height
+      if (quoteImages[index].width > width) width = quoteImages[index].width;
+      height += quoteImages[index].height;
     }
 
-    const quoteMargin = 5
+    const quoteMargin = 5;
 
-    const canvas = createCanvas(width, height + (quoteMargin * quoteImages.length))
-    const canvasCtx = canvas.getContext('2d')
+    const canvas = createCanvas(
+      width,
+      height + quoteMargin * quoteImages.length
+    );
+    const canvasCtx = canvas.getContext("2d");
 
-    let imageY = 0
+    let imageY = 0;
 
     for (let index = 0; index < quoteImages.length; index++) {
-      canvasCtx.drawImage(quoteImages[index], 0, imageY)
-      imageY += quoteImages[index].height + quoteMargin
+      canvasCtx.drawImage(quoteImages[index], 0, imageY);
+      imageY += quoteImages[index].height + quoteMargin;
     }
-    canvasQuote = canvas
+    canvasQuote = canvas;
   } else {
-    canvasQuote = quoteImages[0]
+    canvasQuote = quoteImages[0];
   }
 
-  let quoteImage
+  let quoteImage;
 
-  let { type, format, ext, isWaSticker} = parm
+  let { type, format, ext, isWaSticker } = parm;
 
-  if (!type && ext) type = 'png'
-  if (type !== 'image' && canvasQuote.height > 1024 * 2) type = 'png'
+  if (!type && ext) type = "png";
+  if (type !== "image" && canvasQuote.height > 1024 * 2) type = "png";
 
-  if (type === 'quote') {
-    const downPadding = 75
-    const maxWidth = 512
-    const maxHeight = 512
+  if (type === "quote") {
+    const downPadding = 75;
+    const maxWidth = 512;
+    const maxHeight = 512;
 
-    const imageQuoteSharp = sharp(canvasQuote.toBuffer())
+    const imageQuoteSharp = sharp(canvasQuote.toBuffer());
 
-    if (canvasQuote.height > canvasQuote.width) imageQuoteSharp.resize({ height: maxHeight })
-    else imageQuoteSharp.resize({ width: maxWidth })
+    if (canvasQuote.height > canvasQuote.width)
+      imageQuoteSharp.resize({ height: maxHeight });
+    else imageQuoteSharp.resize({ width: maxWidth });
 
-    const canvasImage = await loadImage(await imageQuoteSharp.toBuffer())
+    const canvasImage = await loadImage(await imageQuoteSharp.toBuffer());
 
-    const canvasPadding = createCanvas(canvasImage.width, canvasImage.height + downPadding)
-    const canvasPaddingCtx = canvasPadding.getContext('2d')
+    const canvasPadding = createCanvas(
+      canvasImage.width,
+      canvasImage.height + downPadding
+    );
+    const canvasPaddingCtx = canvasPadding.getContext("2d");
 
-    canvasPaddingCtx.drawImage(canvasImage, 0, 0)
+    canvasPaddingCtx.drawImage(canvasImage, 0, 0);
 
-    const imageSharp = sharp(canvasPadding.toBuffer())
+    const imageSharp = sharp(canvasPadding.toBuffer());
+    if(isWaSticker){
+      //resize for whatsapp sticker
+      imageSharp.resize({
+        height: 512,
+        width: 512,
+        fit: "contain",
+        background: {
+          r: 0,
+          g: 0,
+          b: 0,
+          alpha: 0,
+        },
+      });
+    } else if (canvasPadding.height >= canvasPadding.width)
+      imageSharp.resize({ height: maxHeight });
+    else imageSharp.resize({ width: maxWidth });
 
-    if (canvasPadding.height >= canvasPadding.width) imageSharp.resize({ height: maxHeight })
-    else imageSharp.resize({ width: maxWidth })
+    if (format === "png") quoteImage = await imageSharp.png().toBuffer();
+    else
+      quoteImage = await imageSharp
+        .webp({ lossless: true, force: true })
+        .toBuffer();
+  } else if (type === "image") {
+    const padding = 25 * parm.scale;
 
-    if (format === 'png') quoteImage = await imageSharp.png().toBuffer()
-    else quoteImage = await imageSharp.webp({ lossless: true, force: true }).toBuffer()
-  } else if (type === 'image') {
-    const padding = 25 * parm.scale
+    const canvasImage = await loadImage(canvasQuote.toBuffer());
 
-    const canvasImage = await loadImage(canvasQuote.toBuffer())
+    const canvasPic = createCanvas(
+      canvasImage.width + padding * 1.7,
+      canvasImage.height + padding * 1.7
+    );
+    const canvasPicCtx = canvasPic.getContext("2d");
 
-    const canvasPic = createCanvas(canvasImage.width + padding * 1.7, canvasImage.height + padding * 1.7)
-    const canvasPicCtx = canvasPic.getContext('2d')
+    canvasPicCtx.fillStyle = lighten(parm.backgroundColor, 20);
+    canvasPicCtx.fillRect(
+      0,
+      0,
+      canvasPic.width + padding,
+      canvasPic.height + padding
+    );
 
-    canvasPicCtx.fillStyle = lighten(parm.backgroundColor, 20)
-    canvasPicCtx.fillRect(0, 0, canvasPic.width + padding, canvasPic.height + padding)
-
-    const canvasPatternImage = await loadImage('./assets/pattern_02.png')
+    const canvasPatternImage = await loadImage("./assets/pattern_02.png");
     // const canvasPatternImage = await loadImage('./assets/pattern_ny.png')
 
-    const pattern = canvasPicCtx.createPattern(canvasPatternImage, 'repeat')
-    canvasPicCtx.fillStyle = pattern
-    canvasPicCtx.fillRect(0, 0, canvasPic.width, canvasPic.height)
+    const pattern = canvasPicCtx.createPattern(canvasPatternImage, "repeat");
+    canvasPicCtx.fillStyle = pattern;
+    canvasPicCtx.fillRect(0, 0, canvasPic.width, canvasPic.height);
 
-    canvasPicCtx.drawImage(canvasImage, padding, padding)
+    canvasPicCtx.drawImage(canvasImage, padding, padding);
 
-    quoteImage = await sharp(canvasPic.toBuffer()).png({ lossless: true, force: true }).toBuffer()
+    quoteImage = await sharp(canvasPic.toBuffer())
+      .png({ lossless: true, force: true })
+      .toBuffer();
   } else {
-    quoteImage = canvasQuote.toBuffer()
+    quoteImage = canvasQuote.toBuffer();
   }
 
-  const imageMetadata = await sharp(quoteImage).metadata()
+  const imageMetadata = await sharp(quoteImage).metadata();
 
-  const width = imageMetadata.width
-  const height = imageMetadata.height
+  const width = imageMetadata.width;
+  const height = imageMetadata.height;
 
-  let image
-  if (ext) image = quoteImage
-  else image = quoteImage.toString('base64')
+  let image;
+  if (ext) image = quoteImage;
+  else image = quoteImage.toString("base64");
 
   return {
     image,
     type,
     width,
     height,
-    ext
-  }
-}
+    ext,
+  };
+};
